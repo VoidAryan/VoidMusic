b027da8ce4460311c81df20fe8c0d1c95bb6fbb0
Priyansh #CrazyBoy <106391059+PriyanshGoswami@users.noreply.github.com>
2023-03-11 21:16:28 +0530

    Update start.py

--- DzL/plugins/bot/start.py
+++ DzL/plugins/bot/start.py
@@ -225,7 +225,7 @@ async def start_comm(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(get_command("START_COMMAND"))
+    filters.command(get_command("VCSTART_COMMAND"))
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
